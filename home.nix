122da2144ace434f73af235d0da1eae2de2bc3fd
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 00:09:55 +0200

    asdf

--- home.nix
+++ home.nix
@@ -1,10 +1,11 @@
 { config, libs, pkgs, userSettings, ... }:
 
+let 
+    gruvboxplus = import ./gruvboxplus.nix { inherit pkgs; };
+in
+
 {
 
-  let 
-    gruvboxplus = import ./gruvboxplus.nix { inherit pkgs; };
-  in {
   gtk.enable = true;
   gtk.cursorTheme.package = pkgs.bibata-cursors;
   gtk.cursorTheme.name = "Bibata-Modern-Ice";
@@ -12,8 +13,7 @@
   gtk.theme.name = "adw-gtk3";
   gtk.iconTheme.package = gruvboxPlus;
   gtk.iconTheme.name = "GruvboxPlus";
-  }
-  
+   
   home.username = userSettings.username;
   home.homeDirectory = "/home/"+userSettings.username;
 
