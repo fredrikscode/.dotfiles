6387e4c7438c989ec5019a459b80368d547fef2b
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 00:02:11 +0200

    add gtk

--- home.nix
+++ home.nix
@@ -1,6 +1,18 @@
 { config, libs, pkgs, userSettings, ... }:
 
 {
+
+  let 
+    gruvboxplus = import ./gruvboxplus.nix { inherit pkgs; };
+  in {
+  gtk.enable = true;
+  gtk.cursorTheme.package = pkgs.bibata-cursors;
+  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3";
+  gtk.iconTheme.package = gruvboxPlus;
+  gtk.iconTheme.name = "GruvboxPlus";
+  }
   
   home.username = userSettings.username;
   home.homeDirectory = "/home/"+userSettings.username;
