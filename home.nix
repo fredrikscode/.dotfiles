a407edab3d8b59bb9101f97ab91de803d6da3725
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 12:08:28 +0200

    remove git config since it's separated

--- home.nix
+++ home.nix
@@ -13,17 +13,6 @@
     neovim
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "fredrikscode";
-    userEmail = "fredrik@kihlstedt.io";
-    extraConfig = {
-      init.defaultBranch = "main";
-      # To avoid git freaking out over dubious permissions
-      safe.directory = "/home/${userSettings.username}/.dotfiles";
-    };
-  };
-
   home.file = {
   };
 
