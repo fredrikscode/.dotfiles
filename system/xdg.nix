29d457c6c8665995f0c72da7c017512c5c8121d8
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 23:27:22 +0200

    add xdg config

---
+++ system/xdg.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+
+{
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
+}
