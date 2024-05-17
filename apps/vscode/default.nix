9c5ae0802ff42ce2c4412fda0e870bf3ab93e08f
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 21:34:01 +0200

    add vscode/default.nix

---
+++ apps/vscode/default.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      equinusocio.vsc-material-theme
+      pkief.material-icon-theme
+      ms-vscode-remote.remote-ssh
+      vscode-extensions.ms-python.python
+    ];
+  };
+
+}
