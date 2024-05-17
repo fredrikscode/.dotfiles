d88b5c3f943a930b76c43c0dd2b2c1491d641aa2
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 23:52:47 +0200

    change mod key quick

--- user/sway/default.nix
+++ user/sway/default.nix
@@ -7,7 +7,7 @@
     wrapperFeatures.gtk = true;
     # Sway-specific Configuration
     config = rec {
-      modifier = "Mod4";
+      modifier = "Shift";
       terminal = "wezterm";
       menu = "wofi --show run";
       # Status bar(s)
