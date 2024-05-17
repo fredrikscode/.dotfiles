8b9963a92f9cf5bd715705b2cbd592bf03225fa5
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 11:28:46 +0200

    Reapply "add nice systemd reload"
    This reverts commit 9e78e61a45d45b34f38826e6306994baa0504d65.

--- home.nix
+++ home.nix
@@ -70,6 +70,9 @@
 
   programs.home-manager.enable = true;
 
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
   home.stateVersion = "23.11";
 
 }
