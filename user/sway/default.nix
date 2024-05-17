ae3900757ef8659319869eb07e0cab1e11d292cb
fredrikscode <fredrik@kihlstedt.io>
2024-05-17 23:57:12 +0200

    change modifier to ctrl

--- user/sway/default.nix
+++ user/sway/default.nix
@@ -7,7 +7,7 @@
     wrapperFeatures.gtk = true;
     # Sway-specific Configuration
     config = rec {
-      modifier = "Shift";
+      modifier = "Ctrl";
       terminal = "wezterm";
       menu = "wofi --show run";
       # Status bar(s)
