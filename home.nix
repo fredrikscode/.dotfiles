bbee29bc97960dbec1f88d2aeee94d9287a0a790
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 09:39:33 +0200

    enable hyprland in home manager

--- home.nix
+++ home.nix
@@ -14,6 +14,19 @@
       allowUnfreePredicate = (_: true);
     };
   };
+
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+    # Optional
+    # Whether to enable hyprland-session.target on hyprland startup
+    systemd.enable = true;
+  };
    
   home.username = userSettings.username;
   home.homeDirectory = "/home/"+userSettings.username;
