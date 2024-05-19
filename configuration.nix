effc27ca25f5b52974d2bcbf5cd6574698fdd6a2
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 22:10:23 +0200

    asdsad

--- configuration.nix
+++ configuration.nix
@@ -36,16 +36,6 @@
     };
   };
 
-  xdg = {
-    portal = {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-      ];
-    };
-  };
-
   nixpkgs = {
     config = {
       allowUnfree = true;
