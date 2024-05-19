d62ab643ac53bc79d8685801e7ba17dc549a4135
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 22:01:53 +0200

    fix formatting

--- configuration.nix
+++ configuration.nix
@@ -9,104 +9,6 @@
       ./system/network.nix
     ];
 
-  time.timeZone = systemSettings.timezone;
-
-  # SDDM
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-
-  # X11
-  #services.xserver.enable = true;
-
-  # Hyprland
-  programs.hyprland.enable = true;
-  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-
-  # Fix electron apps in Wayland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-    # Enable Gnome and GDM
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  environment.variables = {
-    XDG_CONFIG_HOME = "${builtins.getEnv "HOME"}/.dotfiles";
-  };
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${userSettings.username} = {
-    description = "Fredrik";
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "libvirtd" "video" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
-    packages = with pkgs; [
-    ];
-  };
-
-  programs.zsh.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  # Sway dependency
-  #security.polkit.enable = true;
-
-  # Brightness/volume functions keys (primarily for laptops)
-  programs.light.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
   nix = {
     settings = {
       # Enable flakes
@@ -125,7 +27,58 @@
     };
   };
 
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  environment = {
+    variables = {
+      XDG_CONFIG_HOME = "${builtins.getEnv "HOME"}/.dotfiles";
+    };
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+  };
+
+  services = {
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+    };
+    openssh = {
+      enable = true;
+    };
+  };
+
+  programs = {
+    hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    };
+    # Enable laptop brightness keys
+    light = {
+      enable = true;
+    };
+    zsh = {
+      enable = true;
+    };
+  };
+
+  time.timeZone = systemSettings.timezone;
+  
+  users.users.${userSettings.username} = {
+    description = "Fredrik";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" "video" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    ];
+  };
+
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
-
