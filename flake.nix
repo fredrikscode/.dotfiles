7f1d390d4460430b69ad7226c47314d908002494
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 21:45:16 +0200

    add hyprland-plugins

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland-plugins = {
+    url = "github:hyprwm/hyprland-plugins";
+    inputs.hyprland.follows = "hyprland";
+  };
   };
 
   outputs = { self, nixpkgs, home-manager, hyprland, ... }:
