b671587159ba9cf0d127cc6aa19c92f011f16c9c
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 21:40:00 +0200

    enable cachix

--- configuration.nix
+++ configuration.nix
@@ -112,6 +112,9 @@
       experimental-features = [ "nix-command" "flakes" ];
       # Garbage collection
       auto-optimise-store = true;
+      # Cachix
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     # Garbage collection
     gc = {
