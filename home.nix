c48bea776bddc36aee4e9742b68422890328d969
fredrikscode <fredrik@kihlstedt.io>
2024-05-19 09:39:33 +0200

    enable hyprland in home manager

--- home.nix
+++ home.nix
@@ -26,6 +26,23 @@
     # Optional
     # Whether to enable hyprland-session.target on hyprland startup
     systemd.enable = true;
+
+    settings = {
+      decoration = {
+        shadow_offset = "0 5";
+        "col.shadow" = "rgba(00000099)";
+      };
+
+      "$mod" = "SUPER";
+
+      bindm = [
+        # mouse movements
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+      ];
+    };
+    
   };
    
   home.username = userSettings.username;
